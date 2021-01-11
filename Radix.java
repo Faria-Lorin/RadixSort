44618c533105639236b06ebe7b4e78887e02fb86
Faria-Lorin <florin20@stuy.edu>
2021-01-11 13:17:04 -0500

    I added to the nth method, so that using remainder and division, you seperate the desired digit from the rest of the number and you give back the abs valeu of the digit

--- Radix.java
+++ Radix.java
@@ -1,5 +1,16 @@
-public class Radic{
+public class Radix{
   public static int nth(int n, int col){
-    
+    int rem = n%((int)(Math.pow(10, col +1)));
+    if (col == 0){
+
+    }
+    else {
+      rem = rem - rem%((int)(Math.pow(10, col)));
+    }
+    rem = rem / ((int)(Math.pow(10, col)));
+    return Math.abs(rem);
+  }
+  public static void main(String[] args) {
+    System.out.println(nth(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
   }
 }
